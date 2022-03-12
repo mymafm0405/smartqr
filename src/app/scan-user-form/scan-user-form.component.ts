dbdd1f0766c44eca086c3251e15775b9ee382634
Mahmoud <mymafm0405@gmail.com>
2022-03-12 10:32:36 +0300

    change the select list icons

--- src/app/scan-user-form/scan-user-form.component.ts
+++ src/app/scan-user-form/scan-user-form.component.ts
@@ -8,15 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScanUserFormComponent implements OnInit {
   constructor(private usersService: UsersService) {}
+  selectedTarget!: string;
+  startCamera!: boolean;
 
   ngOnInit(): void {}
 
-  onChange(event: any) {
-    console.log(event.target.value);
-    const scanUser = event.target.value;
-    this.usersService.selectedUserChanged.next(scanUser);
-    if (scanUser === '') {
-      this.usersService.changeScan(scanUser);
-    }
+  // onChange(event: any) {
+  //   console.log(event.target.value);
+  //   const scanUser = event.target.value;
+  //   this.usersService.selectedUserChanged.next(scanUser);
+  //   if (scanUser === '') {
+  //     this.usersService.changeScan(scanUser);
+  //   }
+  // }
+  onSelect(option: string) {
+    this.startCamera = false;
+    this.selectedTarget = option;
+    this.usersService.selectedUserChanged.next(this.selectedTarget);
+  }
+
+  onScanClick() {
+    this.startCamera = true;
   }
 }
